89f00e4c958b575ffda4642834b20f0ece33def7
Check-HazratALi <162535361+Check-HazratALi@users.noreply.github.com>
2024-03-23 15:32:44 +0600

    E

---
+++ Interval sweep.cpp
@@ -0,0 +1,17 @@
+    #include <iostream>
+    using namespace std;
+     
+    int main ()
+    {
+        int a, b;
+     
+        cin >> a >> b;
+        if (a==0 && b==0)
+            cout <<"NO";
+        else if( a==b || a==b+1 || b==a+1 )
+            cout << "YES";
+        else
+            cout << "NO";
+     
+        return 0;
+    }
